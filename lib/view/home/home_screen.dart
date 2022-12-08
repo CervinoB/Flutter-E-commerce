0ba50efd0d01cfff33e05d9022d0baa4150c7b92
CervinoB <jcervinobarbosa@gmail.com>
2022-12-08 11:13:50 -0300

    Adding more to the Home Carousel Slider

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:my_grocery/component/main_header.dart';
 import 'package:my_grocery/controller/controllers.dart';
 import 'package:my_grocery/view/home/components/carousel_slider/carousel_loading.dart';
+import 'package:my_grocery/view/home/components/carousel_slider/carousel_slider_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,8 +15,10 @@ class HomeScreen extends StatelessWidget {
         children: [
           const MainHeader(),
           Obx(() {
-            if (homeController.isBannerLoading.value) {
-              return CarouselLoading();
+            if (homeController.bannerList.isNotEmpty) {
+              return CarouselSliderView(
+                bannerList: homeController.bannerList,
+              );
             } else {
               return CarouselLoading();
             }
